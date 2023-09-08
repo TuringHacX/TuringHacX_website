7c6700b593775caa54d77e274b0b793570e479a2
Abhisht <abhishtchouhan@gmail.com>
2023-09-09 01:03:51 +0530

    Attempt to Intergrate Google Anal Failed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// // DONT TOUCH THIS CODE
+// import ReactGA from 'react-ga';
+// const TRACKING_ID = "G-XNHWFTKFLM"; // OUR_TRACKING_ID
+// ReactGA.initialize(TRACKING_ID);
+// // GOOGLE ANALYTICS
 
 import './App.css';
 import Homepage from './pages/Homepage';
